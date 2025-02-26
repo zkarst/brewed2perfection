78515f92bd152579d6c159407d7dccc647194854
Zachary Karst <zacha@LAPTOP-SNHUPLH6>
2025-02-26 14:04:41 -0600

    Edits #2

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 
+    const Bean = document.getElementsByClassName('coffeeBean');
+    const nav = document.getElementsByClassName('nav-item');
+
+   
+    const brownBean = new Image();
+    brownBean.src = 'images/coffeeBean-brown.png';
+
+   
+    const whiteBean = new Image();
+    whiteBean.src = 'images/coffeeBean-white.png';
+
+    
+    if (Bean.length > 0) {
+        
+        console.log("Bean is Brown");
+        
+        Bean[0].src = brownBean.src;
+    } else {
+        
+        console.log("Bean is White");
+     
+    }
+
 
