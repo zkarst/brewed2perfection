ebd292640c846e3841323d02e3e572cb1dad7a71
Zachary Karst <zacha@LAPTOP-SNHUPLH6>
2025-03-03 09:37:05 -0600

    Changes before 3/2/2025

---
+++ includes/mobile-menu.php
@@ -0,0 +1,13 @@
+
+        <div class="mobile-menu" id="mobileMenu">
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                  <img src="/images/menubar.png" id="menuicon">  
+            </a>
+            <ul class="nav-options" id="navbar">
+   <li class="nav"><a href="/">HOME</a></li> 
+   <li class="nav"><a href="/about.php">ABOUT ME</a></li>
+   <li class="nav"><a href="/resume.php">RESUME</a></li>
+   <li class="nav"><a href="/portfolio.php">PORTFOLIO</a></li>
+   <li class="nav"><a href="/contact.php">CONTACT</a></li>
+</ul>
+                </div>
--- js/script.js
+++ js/script.js
@@ -20,6 +20,68 @@
         
         console.log("Bean is White");
      
+}
+
+const boxLeft = document.querySelectorAll(".box-left");
+
+window.addEventListener("scroll", () => {
+    const innerHeightOfWindow = window.innerHeight;
+
+    boxLeft.forEach(box => {
+        // Get the position of the box relative to the viewport
+        const boxPosition = box.getBoundingClientRect().top;
+
+        // If the box is within the viewport, add the "show" class
+        if (boxPosition < innerHeightOfWindow) {
+            box.classList.add("show");
+        } else {
+            box.classList.remove("show");
+        }
+    });
+});
+
+const boxRight = document.querySelectorAll(".box-right");
+
+window.addEventListener("scroll", () => {
+    const innerHeightOfWindow = window.innerHeight;
+
+    boxRight.forEach(box => {
+        // Get the position of the box relative to the viewport
+        const boxPosition = box.getBoundingClientRect().top;
+
+        // If the box is within the viewport, add the "show" class
+        if (boxPosition < innerHeightOfWindow) {
+            box.classList.add("show");
+        } else {
+            box.classList.remove("show");
+        }
+    });
+});
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
+
+}
+function mobileMenu() {
+    if (document.GetElementById('mobileMenu').style.display == 'block') {
+        document.GetElementById('mobileMenu').style.display == 'none';
+    } else {
+        document.GetElementById('mobileMenu').style.display == 'block';
+    }
+
+}
+window.onload = function () {
+
+    if (document.querySelector('#navbar').classList.contains('is-open')) {
+
+        document.querySelector('#navbar').classList.remove('is-open');
+
+    }
+
+};
 
 
